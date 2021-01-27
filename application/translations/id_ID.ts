c423ece7e52117a13c8c66416b19d6530c8f1866
Zumi <daxuya.zumi+code@protonmail.com>
2021-01-27 11:03:02 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 77.7% (21 of 27 strings)
    
    Translation: thePage/thePage Desktop
    Translate-URL: https://translate.vicr123.com/projects/thepage/thepage-desktop/id/

--- application/translations/id_ID.ts
+++ application/translations/id_ID.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../documentviewer.ui" line="60"/>
         <source>Open a document</source>
-        <translation type="unfinished"></translation>
+        <translation>Buka dokumen</translation>
     </message>
     <message>
         <location filename="../documentviewer.ui" line="70"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Buka</translation>
     </message>
     <message>
         <location filename="../documentviewer.ui" line="544"/>
@@ -21,22 +21,22 @@
     <message>
         <location filename="../documentviewer.ui" line="569"/>
         <source>Selection</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleksi</translation>
     </message>
     <message>
         <location filename="../documentviewer.cpp" line="99"/>
         <source>File Not Readable</source>
-        <translation type="unfinished"></translation>
+        <translation>File Tidak Dapat Dibuka</translation>
     </message>
     <message>
         <location filename="../documentviewer.cpp" line="100"/>
         <source>Sorry, that file can&apos;t be opened.</source>
-        <translation type="unfinished"></translation>
+        <translation>Maaf, file yang diminta tidak dapat dibuka.</translation>
     </message>
     <message>
         <location filename="../documentviewer.cpp" line="153"/>
         <source>New Tab</source>
-        <translation type="unfinished"></translation>
+        <translation>Tab Baru</translation>
     </message>
 </context>
 <context>
@@ -44,7 +44,7 @@
     <message>
         <location filename="../mainwindow.ui" line="132"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Keluar</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="135"/>
@@ -54,7 +54,7 @@
     <message>
         <location filename="../mainwindow.ui" line="144"/>
         <source>New Tab</source>
-        <translation type="unfinished"></translation>
+        <translation>Tab Baru</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="147"/>
@@ -64,7 +64,7 @@
     <message>
         <location filename="../mainwindow.ui" line="156"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Buka</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="159"/>
@@ -74,7 +74,7 @@
     <message>
         <location filename="../mainwindow.ui" line="167"/>
         <source>Close Tab</source>
-        <translation type="unfinished"></translation>
+        <translation>Tutup Tab</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="170"/>
@@ -87,47 +87,47 @@
     <message>
         <location filename="../pageviewer.cpp" line="147"/>
         <source>For text %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Untuk teks %1</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="148"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Salin</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="151"/>
         <source>Digital Rights Management</source>
-        <translation type="unfinished"></translation>
+        <translation>Manajemen Hak Digital (DRM)</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="152"/>
         <source>The author of this document prohibits you from copying text and images.</source>
-        <translation type="unfinished"></translation>
+        <translation>Pembuat dokumen tidak memperbolehkan menyalin teks dan gambar.</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="155"/>
         <source>Copy Anyway</source>
-        <translation type="unfinished"></translation>
+        <translation>Tetap Salin</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="194"/>
         <source>Visit Link</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunjungi Link</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="195"/>
         <source>Visit the link %1?</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunjungi link %1?</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="196"/>
         <source>Make sure it&apos;s a place you trust; the web can be scary!</source>
-        <translation type="unfinished"></translation>
+        <translation>Pastikan link tersebut dapat dipercaya!</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="199"/>
         <source>Visit</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunjungi</translation>
     </message>
 </context>
 <context>
@@ -135,7 +135,7 @@
     <message>
         <location filename="../main.cpp" line="40"/>
         <source>Document Viewer</source>
-        <translation type="unfinished"></translation>
+        <translation>Penampil Dokumen</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="66"/>
@@ -145,7 +145,7 @@
     <message>
         <location filename="../main.cpp" line="66"/>
         <source>File to open</source>
-        <translation type="unfinished"></translation>
+        <translation>File yang ingin dibuka</translation>
     </message>
 </context>
 </TS>
