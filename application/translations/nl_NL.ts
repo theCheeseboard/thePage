16cce7fb00f55fcb9806e99ac99d387613e0bf05
Mart Koster <the947thtokay@codingtime.nl>
2021-01-27 12:59:21 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (27 of 27 strings)
    
    Translation: thePage/thePage Desktop
    Translate-URL: https://translate.vicr123.com/projects/thepage/thepage-desktop/nl/

--- application/translations/nl_NL.ts
+++ application/translations/nl_NL.ts
@@ -6,37 +6,37 @@
     <message>
         <location filename="../documentviewer.ui" line="60"/>
         <source>Open a document</source>
-        <translation type="unfinished"></translation>
+        <translation>Een document openen</translation>
     </message>
     <message>
         <location filename="../documentviewer.ui" line="70"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Openen</translation>
     </message>
     <message>
         <location filename="../documentviewer.ui" line="544"/>
         <source>Scrolling</source>
-        <translation type="unfinished"></translation>
+        <translation>Scrollen</translation>
     </message>
     <message>
         <location filename="../documentviewer.ui" line="569"/>
         <source>Selection</source>
-        <translation type="unfinished"></translation>
+        <translation>Selecteren</translation>
     </message>
     <message>
         <location filename="../documentviewer.cpp" line="99"/>
         <source>File Not Readable</source>
-        <translation type="unfinished"></translation>
+        <translation>Bestand Niet Leesbaar</translation>
     </message>
     <message>
         <location filename="../documentviewer.cpp" line="100"/>
         <source>Sorry, that file can&apos;t be opened.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sorry, dat bestand kan niet worden geopend.</translation>
     </message>
     <message>
         <location filename="../documentviewer.cpp" line="153"/>
         <source>New Tab</source>
-        <translation type="unfinished"></translation>
+        <translation>Nieuw Tabblad</translation>
     </message>
 </context>
 <context>
@@ -44,42 +44,42 @@
     <message>
         <location filename="../mainwindow.ui" line="132"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Sluiten</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="135"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Q</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="144"/>
         <source>New Tab</source>
-        <translation type="unfinished"></translation>
+        <translation>Nieuw Tabblad</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="147"/>
         <source>Ctrl+T</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+T</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="156"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Openen</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="159"/>
         <source>Ctrl+O</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+O</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="167"/>
         <source>Close Tab</source>
-        <translation type="unfinished"></translation>
+        <translation>Tabblad Sluiten</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="170"/>
         <source>Ctrl+W</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+W</translation>
     </message>
 </context>
 <context>
@@ -87,47 +87,47 @@
     <message>
         <location filename="../pageviewer.cpp" line="147"/>
         <source>For text %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Voor tekst %1</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="148"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopiëren</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="151"/>
         <source>Digital Rights Management</source>
-        <translation type="unfinished"></translation>
+        <translation>Digitaal Rechtenbeheer</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="152"/>
         <source>The author of this document prohibits you from copying text and images.</source>
-        <translation type="unfinished"></translation>
+        <translation>De auteur van dit document verbiedt het kopiëren van tekst en afbeeldingen.</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="155"/>
         <source>Copy Anyway</source>
-        <translation type="unfinished"></translation>
+        <translation>Toch Kopiëren</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="194"/>
         <source>Visit Link</source>
-        <translation type="unfinished"></translation>
+        <translation>Link Bezoeken</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="195"/>
         <source>Visit the link %1?</source>
-        <translation type="unfinished"></translation>
+        <translation>De link %1 bezoeken?</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="196"/>
         <source>Make sure it&apos;s a place you trust; the web can be scary!</source>
-        <translation type="unfinished"></translation>
+        <translation>Zorg ervoor dat je die plek vertrouwt; het web kan eng zijn!</translation>
     </message>
     <message>
         <location filename="../pageviewer.cpp" line="199"/>
         <source>Visit</source>
-        <translation type="unfinished"></translation>
+        <translation>Bezoeken</translation>
     </message>
 </context>
 <context>
@@ -135,17 +135,17 @@
     <message>
         <location filename="../main.cpp" line="40"/>
         <source>Document Viewer</source>
-        <translation type="unfinished"></translation>
+        <translation>Documentlezer</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="66"/>
         <source>file</source>
-        <translation type="unfinished"></translation>
+        <translation>bestand</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="66"/>
         <source>File to open</source>
-        <translation type="unfinished"></translation>
+        <translation>Te openen bestand</translation>
     </message>
 </context>
 </TS>
